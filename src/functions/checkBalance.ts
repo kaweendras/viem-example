ae3b871d4152774796de433071e4e27a7b197acf
kaweendras <salitha.wijerathna@gmail.com>
2024-05-29 12:33:52 +0530

    CHG |

--- src/functions/checkBalance.ts
+++ src/functions/checkBalance.ts
@@ -1,15 +1,13 @@
 import dotenv from "dotenv";
 
 import { formatEther } from "viem";
-import { createClient } from "./utils/createClient";
-
+import { createClient } from "../utils/createClient";
 
 dotenv.config();
 
 const accountAddress = process.env.ACCOUNT_ADDRESS;
 
-const balance = async () => {
-
+const checkBalance = async () => {
   const client = await createClient();
 
   const balance = await client.getBalance({
@@ -18,7 +16,6 @@ const balance = async () => {
 
   const formattedBalance = formatEther(balance);
   console.log("Balance -", formattedBalance);
-
 };
 
-balance();
+export default checkBalance;
