dbdec9572a29263e5876a3d572fe613d8a6d844a
kaweendras <salitha.wijerathna@gmail.com>
2024-05-29 12:23:41 +0530

    CHG | importing createClient

--- src/checkBalance.ts
+++ src/checkBalance.ts
@@ -1,17 +1,13 @@
 import dotenv from "dotenv";
-import { createPublicClient, formatEther, http } from "viem";
-import { arbitrumSepolia } from "viem/chains";
+import { formatEther } from "viem";
+import { createClient } from "./utils/createClient";
 
 dotenv.config();
 
 const accountAddress = process.env.ACCOUNT_ADDRESS;
 
 const balance = async () => {
-  const client = await createPublicClient({
-    chain: arbitrumSepolia,
-    transport: http(process.env.API_KEY),
-  });
-
+  const client = await createClient();
   const balance = await client.getBalance({
     address: accountAddress as `0x${string}`,
   });
