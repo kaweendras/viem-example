fd933c00d9cff0acfa63cc791046fa51e82c5ed1
kaweendras <salitha.wijerathna@gmail.com>
2024-05-30 11:17:52 +0530

    CHG | Events

--- src/contracts/Fun.sol
+++ src/contracts/Fun.sol
@@ -3,11 +3,15 @@ pragma solidity 0.8.26;
 
 contract Fun {
     uint256 public x;
+
+    event XChanged(uint _from , uint _to);
     constructor(uint256 _x) {
+        emit XChanged(x, _x);
         x = _x;        
     }
 
     function changeX(uint256 _x) public {
+        emit XChanged(x, _x);
         x = _x;
     }
 
