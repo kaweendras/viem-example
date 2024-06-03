bd37c01103cda644657de9bb3153c7bd45708df1
kaweendras <salitha.wijerathna@gmail.com>
2024-06-03 10:56:28 +0530

    CHG | Export ting functions and importing to index

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,15 @@
 import checkBalance from "./checkBalance";
 import checkTransactionCount from "./checkTransactionCount";
+import readContract from "./readContract";
+import readContractOld from "./readContract.old";
+import events from "./watchEvent";
+import writeContract from "./writeContract";
 
-export { checkBalance, checkTransactionCount };
+export {
+  checkBalance,
+  checkTransactionCount,
+  readContract,
+  readContractOld,
+  events,
+  writeContract,
+};
--- src/functions/watchEvent.ts
+++ src/functions/watchEvent.ts
@@ -24,3 +24,5 @@ const events = async () => {
 };
 
 events();
+
+export default events;
