a2d0a3c0a36084d4bc76b90b4c68325e52e63927
kaweendras <salitha.wijerathna@gmail.com>
2025-02-06 10:00:37 +0530

    CHG | Add functionality to save account details to a JSON file

--- src/utils/createAccount.ts
+++ src/utils/createAccount.ts
@@ -1,6 +1,8 @@
 import crypto from "crypto";
 import { Hex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+import fs from "fs";
+import path from "path";
 
 //creating new private key
 
@@ -12,6 +14,26 @@ const createAccount = () => {
   console.log("Private Key -", formattedPrivateKey);
   console.log("Account Address -", account.address);
   console.log("Account Public Key -", account.publicKey);
+
+try {
+  fs.writeFileSync(
+    path.join(__dirname, "../artifacts", `accounts-${Date.now()}.json`),
+    JSON.stringify({
+      privateKey: formattedPrivateKey,
+      address: account.address,
+      publicKey: account.publicKey,
+    })
+  );
+} catch (error) {
+  console.error("Error writing account to file:", error);
+}
+
+
+
+
+
+
+
 
   return {
     privateKey: formattedPrivateKey,
